4bc16778971a58f0a091766671c13149ab3dacce
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-25 16:00:45 +0000

    Checking in guest

--- room.rb
+++ room.rb
@@ -1,11 +1,18 @@
 class Room
 
-  attr_reader :theme
+  attr_reader :theme, :customers
 
   def initialize(theme)
     @theme = theme
-    @cutomers = []
+    @customers = []
     @songs = []
   end
 
+  def checking_in_guest(guest)
+    return @customers.push(guest)
+  end
+
+  # def customers_in_the_room
+  #   return @customers.length
+  # end
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,6 +1,7 @@
 require('minitest/autorun')
 require("minitest/rg")
 require_relative("../room.rb")
+require_relative("../guest.rb")
 
 class RoomTest < MiniTest::Test
 
@@ -8,9 +9,26 @@ class RoomTest < MiniTest::Test
     @room_abba = Room.new("Abba")
     @room_rock = Room.new("Rock")
     @room_oldschool = Room.new('Oldschool')
+    @guest_1 = Guest.new("Emil")
   end
 
   def test_room_theme
     assert_equal("Abba", @room_abba.theme)
   end
+
+  def test_customers_in_the_room_empty
+    assert_equal(0, @room_abba.customers.length)
+  end
+
+  def test_custmers_in_the_room_with_guest
+    @room_abba.checking_in_guest(@guest_1)
+    assert_equal(1, @room_abba.customers.length)
+  end
+  # def test_cutomers_in_the_room_with_quest
+  #   asset_equal (1, @room_abba.customers.length)
+  # end
+  # def test_checking_in_guest
+  #   checking_in_guest(@room_abba)
+  #   assert_equal(1, @room_abba.customers_in_the_room)
+  # end
 end
