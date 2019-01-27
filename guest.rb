6d47bcb288d6cc0015b7a5113b72940a6cf64cc7
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-27 22:50:32 +0000

    Checking fee affordability tested

--- guest.rb
+++ guest.rb
@@ -1,10 +1,18 @@
 class Guest
 
-  attr_reader :name, :favourite_song
+  attr_reader :name, :favourite_song, :wallet
 
-  def initialize(name, favourite_song)
+  def initialize(name, favourite_song, wallet)
     @name = name
     @favourite_song = favourite_song
+    @wallet = wallet
   end
 
+  def guest_can_afford_fee(room)
+    if room.entry_fee <= @wallet
+      @wallet -= room.entry_fee
+    else
+      p "Sorry pal, too expensive"
+    end
+  end
 end
--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -1,13 +1,15 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../guest.rb")
+require_relative("../room.rb")
 
 class GuestTest < MiniTest::Test
 
   def setup
-    @guest_1 = Guest.new("Emil", "Thriller")
-    @guest_2 = Guest.new("John", "November rain")
-    @guest_3 = Guest.new("Peter", "Like a pryer")
+    @guest_1 = Guest.new("Emil", "Thriller", 5.00)
+    @guest_2 = Guest.new("John", "November rain", 4.00)
+    @guest_3 = Guest.new("Peter", "Like a pryer", 7.00)
+    @room_abba = Room.new("Abba")
   end
 
 
@@ -19,4 +21,17 @@ class GuestTest < MiniTest::Test
     assert_equal("Thriller", @guest_1.favourite_song)
   end
 
+  def test_customer_wallet
+    assert_equal(5.00, @guest_1.wallet)
+    assert_equal(7.00, @guest_3.wallet)
+  end
+
+  def test_guest_can_afford_fee
+    assert_equal(0, @guest_1.guest_can_afford_fee(@room_abba))
+    assert_equal(2, @guest_3.guest_can_afford_fee(@room_abba))
+  end
+
+  # def test_customer_cannot_afford_fee
+  #   assert_equal("Sorry pal, too expensive", @guest_2.guest_can_afford_fee(@room_abba))
+  # end
 end
